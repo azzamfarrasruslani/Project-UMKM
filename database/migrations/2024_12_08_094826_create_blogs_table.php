58e3f12c00e8377e215283f639446e497ededd67
Sariiw <triwulandarijunitasari@gmail.com>
2024-12-08 20:00:07 +0700

    menambahkan blog migration

--- database/migrations/2024_12_08_094826_create_blogs_table.php
+++ database/migrations/2024_12_08_094826_create_blogs_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('blogs',function (Blueprint $table) {
             $table->id('id_blog');
             $table->string('judul_blog');
             $table->text('konten_blog');
