3247aedb2888243a326d984ae436017bba274618
Sariiw <triwulandarijunitasari@gmail.com>
2024-12-20 10:24:08 +0700

    "controller tentang kami"

---
+++ app/Http/Controllers/TentangKamiController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TentangKami;
+use Illuminate\Http\Request;
+
+class TentangKamiController extends Controller
+{
+
+    public function index()
+    {
+        $tentangKamis = TentangKami::all();  
+        return view('tentangkami.index', compact('tentangKamis'));
+    }
+
+    public function create()
+    {
+        return view('tentangKami.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'judul_tK' => 'required|string|max:255',
+            'deskripsi_tK' => 'required',
+            'gambar_tK' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('gambar_tK')) {
+            $validated['gambar_tK'] = $request->file('gambar_tK')->store('uploads', 'public');
+        }
+
+        TentangKami::create($validated);
+
+        return redirect()->route('tentangKami.index')->with('success', 'Data berhasil ditambahkan.');
+    }
+
+    public function show(TentangKami $tentangKami)
+    {
+        return view('tentangKami.show', compact('tentangKami'));
+    }
+
+    public function edit(TentangKami $tentangKami)
+    {
+
+        return view('tentangKami.edit', compact('tentangKami'));
+    }
+
+    public function update(Request $request, TentangKami $tentangKami)
+    {
+
+        $validated = $request->validate([
+            'judul_tK' => 'required|string|max:255',
+            'deskripsi_tK' => 'required',
+            'gambar_tK' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('gambar_tK')) {
+            $validated['gambar_tK'] = $request->file('gambar_tK')->store('uploads', 'public');
+        }
+
+        $tentangKami->update($validated);
+        return redirect()->route('tentangKami.index')->with('success', 'Data berhasil diperbarui.');
+    }
+
+    public function destroy(TentangKami $tentangKami)
+    {
+        $tentangKami->delete();
+        return redirect()->route('tentangKami.index')->with('success', 'Data berhasil dihapus.');
+    }
+}
