692372cc2391248bdad1094b1b3ca98a1161805d
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2024-12-21 14:48:19 +0700

    Menambahkan menu "Pengaturan Home" dengan submenu "Hero" di sideNav.blade.php

--- resources/views/layouts/sideNav.blade.php
+++ resources/views/layouts/sideNav.blade.php
@@ -1,9 +1,10 @@
 <!-- sidenav  -->
+
 <aside
     class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-2  overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-2xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:left-0 xl:translate-x-0"
     aria-expanded="false">
     <div class="flex flex-col h-full">
-        <div class="sticky top-0 h-20">
+        <div class="sticky top-0 h-20 z-50 bg-white dark:bg-slate-850">
             <i class="hidden absolute top-0 right-0 p-4 cursor-pointer fas fa-times dark:text-white text-slate- bg-opacity-20 xl:hidden"
                 sidenav-close></i>
             <a class="flex justify-center px-4 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700"
@@ -15,32 +16,30 @@
         <hr
             class="h-px mt-4 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
 
-        <div class="items-center block w-auto max-h-screen  grow basis-full  overflow-hidden">
-            <ul class="flex flex-col pl-0 mb-0">
+        <div class="items-center block w-auto max-h-screen grow basis-full overflow-y-visible">
+            <ul class="flex flex-col pl-0 mb-0 h-full">
                 <!-- Dashboard -->
                 <li class="mt-0.5 w-full">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         <div
                             class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                             <x-icon-link :active="request()->routeIs('dashboard')">
-                                <i
-                                    class="relative top-0 text-sm leading-normal fa-solid fa-gauge fa-lg"></i>
+                                <i class="relative top-0 text-sm leading-normal fa-solid fa-gauge fa-lg"></i>
                             </x-icon-link>
                         </div>
                         <span
                             class="ml-1 duration-300 opacity-100 pointer-events-none ease">{{ __('Dashboard') }}</span>
                     </x-nav-link>
                 </li>
-
                 <li class="w-full mt-4">
-                    <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Data</h6>
+                    <h6 class="pl-3 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Data</h6>
                 </li>
                 <!-- Menu -->
                 <li class="mt-0.5 w-full">
                     <x-nav-link :href="route('menu.index')" :active="request()->routeIs('menu.index')">
                         <div
                             class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                            <i class="fa-solid fa-utensils fa-lg" ></i>
+                            <i class="fa-solid fa-utensils fa-lg"></i>
 
                         </div>
                         <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Menu</span>
@@ -58,7 +57,9 @@
                 </li>
                 {{-- Outlet --}}
                 <li class="mt-0.5 w-full">
-                    <x-nav-link :href="route('outlet.index')" :active="request()->routeIs('outlet.index') ||request()->routeIs('outlet.detail')||request()->routeIs('outlet.edit') " >
+                    <x-nav-link :href="route('outlet.index')" :active="request()->routeIs('outlet.index') ||
+                        request()->routeIs('outlet.detail') ||
+                        request()->routeIs('outlet.edit')">
                         <div
                             class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                             <i class="fa-solid fa-building fa-lg"></i>
@@ -67,56 +68,50 @@
                     </x-nav-link>
                 </li>
 
-                 {{-- Blog--}}
-                 <li class="mt-0.5 w-full">
+                {{-- Blog --}}
+                <li class="mt-0.5 w-full">
                     <x-nav-link :href="route('blog.index')" :active="request()->routeIs('blog.index')">
                         <div
                             class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                             <i class="fa-solid fa-newspaper fa-lg"></i>
                         </div>
-                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease" >Blog</span>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Blog</span>
                     </x-nav-link>
                 </li>
-
-
-
-                <!-- <li class="mt-0.5 w-full">
-                    <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap  px-4  transition-colors dark:text-white dark:opacity-80
-                        " href="">
-                        <div
-                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                            <i class="fa-solid fa-egg fa-lg" style="color: #63E6BE;"></i>
-                        </div>
-                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Telur</span>
-                    </a>
-                </li> -->
-
-                <!-- Data Telur -->
-                <li class="mt-0.5 w-full">
-                    {{-- <x-nav-link :href="route('telur.index')" :active="request()->routeIs('telur.index')">
-                        <div
-                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                            <i class="fa-solid fa-egg fa-lg" style="color: #63E6BE;"></i>
-                        </div>
-                        <span
-                            class="ml-1 duration-300 opacity-100 pointer-events-none ease">{{ __('Data Telur') }}</span>
-                    </x-nav-link> --}}
-                </li>
-
-                <!-- Inventori Kandang -->
-                <li class="mt-0.5 w-full">
-                    {{-- <x-nav-link :href="route('inventori.index')" :active="request()->routeIs('inventori.index')">
-                        <div
-                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                            <i class="fa-solid fa-cart-flatbed" style="color: #FFD43B;"></i>
-                        </div>
-                        <span
-                            class="ml-1 duration-300 opacity-100 pointer-events-none ease">{{ __('Inventori Kandang') }}</span>
-                    </x-nav-link> --}}
-                </li>
-
                 <li class="w-full mt-4">
-                    <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Akun</h6>
+                    <h6 class="pl-3 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Pengaturan</h6>
+                </li>
+                <!-- Hero -->
+                <li class="mt-0.5 w-full">
+                    <div x-data="{ open: false }" class="relative">
+                        <button @click="open = !open"
+                            class="flex items-center justify-between w-full px-4 py-2 text-left hover:bg-customRed-50 hover:rounded-lg hover:text-white dark:hover:bg-slate-700">
+                            <div class="flex items-center">
+                                <div
+                                    class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                    <i class="fa-solid fa-house-signal fa-lg"></i>
+                                </div>
+                                <span class="ml-1 duration-300 opacity-100 text-sm pointer-events-none ease">Pengaturan
+                                    Home</span>
+                            </div>
+                            <i :class="open ? 'fa-chevron-up' : 'fa-chevron-down'" class="fa-solid"></i>
+                        </button>
+                        <ul x-show="open" x-cloak x-transition
+                            class="mt-2 space-y-2  rounded-lg  dark:bg-slate-800">
+                            <li class="mt-0.5 w-full">
+                                <x-nav-link :href="route('hero.index')" :active="request()->routeIs('hero.index')">
+                                    <div
+                                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                        <i class="fa-solid fa-image"></i>
+                                    </div>
+                                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Hero</span>
+                                </x-nav-link>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="w-full mt-4">
+                    <h6 class="pl-3 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Akun</h6>
                 </li>
 
                 <li class="mt-0.5 w-full">
@@ -128,13 +123,10 @@
                         <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">{{ __('Profile') }}</span>
                     </x-nav-link>
                 </li>
-                <form method="POST" action="{{ route('logout') }}" class="lg:mt-30 mt-10">
+                <form method="POST" action="{{ route('logout') }}" class="mt-auto">
                     @csrf
-                    <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80 "
-                        href="route('logout')"
-                        onclick="event.preventDefault();
-                                            this.closest('form').submit();">
-
+                    <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80"
+                        href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                         <div
                             class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                             <i class="fa-solid fa-right-from-bracket"></i>
